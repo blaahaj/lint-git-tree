661194afef6bd1f766faf12b81c0ca6d6dc0263c
Rachel Evans <rachel@rachelevans.org>
2025-08-13 10:05:32 +0200

    chore: bring lint rules closer to standard

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import importSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -10,6 +11,24 @@ export default tseslint.config(
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+  },
+  {
+    plugins: { importSort },
+  },
+  {
+    rules: {
+      // These rules mostly seem good at hiding the real cause of
+      // the error in a much less comprehensible error message.
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-comparison": "off",
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-unary-minus": "off",
     },
   },
   {
